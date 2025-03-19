ffc37c5b051b0e712357f61018aca132f3106395
Rishabh-Saxena1 <rishabhsaxena577@gmail.com>
2025-03-19 11:03:53 +0530

    Dto and ModelMapper

---
+++ src/main/java/com/example/workshop_addressbook/dto/AddressBookDTO.java
@@ -0,0 +1,24 @@
+package com.example.workshop_addressbook.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddressBookDTO {
+
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Pattern(regexp = "\\d{10}", message = "Phone number must be 10 digits")
+    private String phoneNumber;
+
+    @NotBlank(message = "Address cannot be empty")
+    private String address;
+}
